6aff84101c0fc48801006542509c5e7da433cff1
ayeshaamin <ayeshaamin470@gmail.com>
2021-03-28 23:54:36 +0600

    new

---
+++ Final Lab 2/dashbord.php
@@ -0,0 +1,19 @@
+
+
+
+
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<h1><i> Choose your option</i></h1>
+<a href="http://localhost/phpmyadmin/index.php?route=/sql&server=1&db=wt_sp21_l&table=student&pos=0" class="button">Student</a><br>
+
+    <a href="AllStudent.php">AllStudent</a><br>
+	<a href="http://localhost/phpmyadmin/index.php?route=/sql&server=1&db=wt_sp21_l&table=department&pos=0" class="button">Department</a><br>
+
+    <a href="AddStudent.php">AddStudent</a>
+</body>
+</html>
